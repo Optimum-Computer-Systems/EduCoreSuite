105dd1dfb736f347fbdd090e8fbe72a62c40974b
nickssilver <silvernicks9@gmail.com>
2025-06-10 14:09:31 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EduCoreSuite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduCoreSuite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Add model configurations here if needed
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using EduCoreSuite.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ?? Add services to the container
+builder.Services.AddRazorPages();
+
+// ? Register ApplicationDbContext with SQL Server connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// ?? Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization(); // If you use authentication, add `app.UseAuthentication()` before this
+
+app.MapRazorPages();
+
+app.Run();
