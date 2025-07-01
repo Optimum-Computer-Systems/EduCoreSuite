4422976ef5e9e93ef0e9e5e3170e31cddad56ff9
Gerry-Migiro <gersonmigiro@gmail.com>
2025-07-01 22:28:52 +0300

    Updated program.cs

--- Program.cs
+++ Program.cs
@@ -1,31 +1,41 @@
-using EduCoreSuite.Data;
 using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// ?? Add services to the container
-builder.Services.AddRazorPages();
-
-// ? Register ApplicationDbContext with SQL Server connection
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
-
-// ?? Configure the HTTP request pipeline
-if (!app.Environment.IsDevelopment())
+using Optimum_University.Data;
+using System;
+namespace Optimum_University
 {
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDBContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization(); // If you use authentication, add `app.UseAuthentication()` before this
-
-app.MapRazorPages();
-
-app.Run();
