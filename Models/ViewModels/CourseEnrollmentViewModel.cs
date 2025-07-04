1f0540c8564d763368f7c687ce8a2be5dc7bf7ad
Gerry-Migiro <gersonmigiro@gmail.com>
2025-07-04 11:53:34 +0300

    Adjusted Course Enrollment View Model

--- Models/ViewModels/CourseEnrollmentViewModel.cs
+++ Models/ViewModels/CourseEnrollmentViewModel.cs
@@ -1,25 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
-using EduCoreSuite.Models;
 
 namespace EduCoreSuite.Models.ViewModels
 {
     public class CourseEnrollmentViewModel
     {
-      
-        public Course Course { get; set; } = new Course();
-
-
-        public List<SelectListItem> Departments { get; set; } = new();
-        public List<SelectListItem> Programmes { get; set; } = new();
-        public List<SelectListItem> StudyLevels { get; set; } = new();
-        public List<SelectListItem> Campuses { get; set; } = new();
-        public List<SelectListItem> ExamBodies { get; set; } = new();
-        public List<SelectListItem> StudyStatuses { get; set; } = new();
-
-        // Additional enrollment-specific fields
         public int SelectedStudentId { get; set; }
         public List<int> SelectedStudentIds { get; set; } = new();
+        public int SelectedCourseId { get; set; }
+        public List<int> SelectedCourseIds { get; set; } = new();
+        public string SelectedDepartment { get; set; } = "";
+        public string SelectedCampus { get; set; } = "";
+        public string SelectedYear { get; set; } = "";
+
         public List<SelectListItem> Students { get; set; } = new();
+        public List<SelectListItem> Courses { get; set; } = new();
+        public List<SelectListItem> Campuses { get; set; } = new();
+        public List<SelectListItem> Years { get; set; } = new();
     }
 }
