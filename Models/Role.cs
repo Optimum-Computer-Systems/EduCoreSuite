6794211eebad90d6bfe99fbeaa2916c5b0d0e85a
Hamisi_Juma <hamisig123@gmail.com>
2025-07-08 21:36:24 +0300

    Created user and role model and added relationships

---
+++ Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EduCoreSuite.Models
+{
+    public class Role
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String? Description { get; set; }
+        [JsonIgnore]
+        public ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace EduCoreSuite.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        [Required]
+        public String Username { get; set; }
+
+        public int RoleID { get; set; }
+        [ForeignKey("RoleID")]
+        public Role Role { get; set; }
+    }
+}
