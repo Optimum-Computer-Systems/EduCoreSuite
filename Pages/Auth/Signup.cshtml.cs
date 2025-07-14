3df7e5028a046e83b0e83c411ed2afd2720cab3f
Hamisi_Juma <hamisig123@gmail.com>
2025-07-14 18:39:52 +0300

    Worked on the logic for the sign up and the front end of the page. Still work in progress...

--- Pages/Auth/Signup.cshtml.cs
+++ Pages/Auth/Signup.cshtml.cs
@@ -2,6 +2,7 @@ using EduCoreSuite.Data;
 using Microsoft.AspNetCore.Mvc;
 using EduCoreSuite.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduCoreSuite.Pages.Auth
 {
@@ -18,14 +19,26 @@ namespace EduCoreSuite.Pages.Auth
         {
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
-            //if (!ModelState.IsValid)
-            //{
-    
-            //}
+            if (await _db.Users.AnyAsync(u => u.Username == User.Username))
+            {
+                ModelState.AddModelError("User.Username", "Username already taken");
+                return Page();
+            }
+
+            if (await _db.Users.AnyAsync(u => u.Email == User.Email))
+            {
+                ModelState.AddModelError("User.Email", "Email already taken");
+                return Page();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
             _db.Add(User);
-            _db.SaveChanges();
+            await _db.SaveChangesAsync();
             return Page();
         }
     }
