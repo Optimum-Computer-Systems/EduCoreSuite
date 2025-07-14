d8e61c680f560c63031875042ab90526ede6360d
Gerry-Migiro <gersonmigiro@gmail.com>
2025-07-14 16:25:56 +0300

    Updated Students Model; County and subcounty FKs columns

--- Migrations/20250714130944_AddCountyAndSubCountyFKsToStudent.Designer.cs
+++ Migrations/20250714130944_AddCountyAndSubCountyFKsToStudent.Designer.cs
@@ -4,6 +4,7 @@ using EduCoreSuite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EduCoreSuite.Migrations
 {
     [DbContext(typeof(ForgeDBContext))]
-    partial class ForgeDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250714130944_AddCountyAndSubCountyFKsToStudent")]
+    partial class AddCountyAndSubCountyFKsToStudent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
