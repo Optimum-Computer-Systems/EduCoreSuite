8d9c75bc4676df5d9bfa3284da28a884715baeb0
Hamisi_Juma <hamisig123@gmail.com>
2025-07-14 19:24:30 +0300

    Password visibility toggle JS script

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,2 +1,23 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 
+//For password visibility toggle
+const togglePassword = document.getElementById('togglePassword');
+const togglePasswordIcon = document.getElementById('togglePasswordIcon');
+const passwordInput = document.getElementById('Password');
+
+togglePassword.addEventListener('click', function () {
+    let type = passwordInput.getAttribute('type');
+
+    if (type === 'password') {
+        type = 'text';
+    }
+    else {
+        type = 'password';
+    }
+
+    passwordInput.setAttribute('type', type);
+    //toggle icon
+    togglePasswordIcon.classList.toggle('bi-eye');
+    togglePasswordIcon.classList.toggle('bi-eye-slash');
+})
+
