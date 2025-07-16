536788a730b4d21bfc1372edd118c4d8e5cbb7ee
owuorviny109 <owuorvincent958@gmail.com>
2025-07-16 11:55:13 +0300

    resolved course submit button

--- Models/Course.cs
+++ Models/Course.cs
@@ -16,27 +16,27 @@ namespace EduCoreSuite.Models
         // Foreign Keys & Navigation Properties
         [Display(Name = "Department")]
         public int DepartmentID { get; set; }
-        public Department Department { get; set; } = null!;
+        public Department? Department { get; set; }
 
         [Display(Name = "Programme")]
         public int ProgrammeID { get; set; }
-        public Programme Programme { get; set; } = null!;
+        public Programme? Programme { get; set; }
 
         [Display(Name = "Campus")]
         public int CampusID { get; set; }
-        public Campus Campus { get; set; } = null!;
+        public Campus? Campus { get; set; }
 
         [Display(Name = "Exam Body")]
         public int ExamBodyID { get; set; }
-        public ExamBody ExamBody { get; set; } = null!;
+        public ExamBody? ExamBody { get; set; }
 
         [Display(Name = "Study Status")]
         public int StudyStatusID { get; set; }
-        public StudyStatus StudyStatus { get; set; } = null!;
+        public StudyStatus? StudyStatus { get; set; }
 
         [Display(Name = "Study Mode")]
         public int StudyModeID { get; set; }
-        public StudyMode StudyMode { get; set; } = null!;
+        public StudyMode? StudyMode { get; set; }
 
         public Course() { }
     }
